12c804cb3da770b537926a4cd64bce78ab3000e9
Anderson Joyle <me@andersonjoyle.com>
2017-10-08 14:29:01 -0700

    Adding "make me laugh".
    Just a small project to cool down the stress.

---
+++ D365O_Addin_MakeMeLaugh/Addin/MainMenuAddIn.cs
@@ -0,0 +1,66 @@
+﻿namespace Addin
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.Composition;
+    using Microsoft.Dynamics.Framework.Tools.Extensibility;
+    using Microsoft.Dynamics.Framework.Tools.MetaModel.Core;
+
+    using Joking;
+
+    /// <summary>
+    /// Display a Chuck Norris random joke.
+    /// </summary>
+    [Export(typeof(IMainMenu))]
+    public class MainMenuAddIn : MainMenuBase
+    {
+        #region Member variables
+        private const string addinName = "Addin";
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Caption for the menu item. This is what users would see in the menu.
+        /// </summary>
+        public override string Caption
+        {
+            get
+            {
+                return AddinResources.MainMenuAddInCaption;
+            }
+        }
+
+        /// <summary>
+        /// Unique name of the add-in
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return MainMenuAddIn.addinName;
+            }
+        }
+
+        #endregion
+
+        #region Callbacks
+        /// <summary>
+        /// Called when user clicks on the add-in menu
+        /// </summary>
+        /// <param name="e">The context of the VS tools and metadata</param>
+        public override void OnClick(AddinEventArgs e)
+        {
+            try
+            {
+                JokeEngine engine = new JokeEngine();
+
+                CoreUtility.DisplayInfo(engine.makeMeLaugh());
+            }
+            catch (Exception ex)
+            {
+                CoreUtility.HandleExceptionWithErrorMessage(ex);
+            }
+        }
+        #endregion
+    }
+}
---
+++ D365O_Addin_MakeMeLaugh/Addin/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("D365O Addin - Make me laugh!")]
+[assembly: AssemblyDescription("When you are under pressure, nothing better to relax with a joke!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/anderson-joyle/D365O-Addins")]
+[assembly: AssemblyProduct("D365O Addin")]
+[assembly: AssemblyCopyright("GNU GPLv3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd4600c6-8df3-4d4e-b6b5-ea27c1949995")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+
